dd58a4155003b533b80a21f6cc3efb2b0e592d2d
HladkyiSS <sergii.gladkyi.01@gmail.com>
2024-06-04 14:15:43 +0300

    Update catalog/admin.py to create and display fields in admin panel from Book Author and Format databases

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from catalog.models import Book, Author, Format
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ["title", "format"]
+    list_filter = ["format"]
+    search_fields = ["title"]
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = admin.ModelAdmin.list_display + ("pseudonym",)
+
+
+admin.site.register(Format)
